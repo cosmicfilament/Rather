852a3eebcc83311946e9fcf1828d3831ea9c80fc
cosmicfilament <john@thecosmicfilament.com>
2018-12-28 07:44:54 -0500

    initial install

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>React Redux Boilerplate</h1>
+                <h1>Would You Rather?</h1>
             </div>
         );
     }
